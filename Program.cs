a4347a51a12fb7ff333c58b24954625c8ba67d20
DFY73IER <leshaar14@gmail.com>
2021-01-24 19:40:06 +0100

    Revert "add mwthod"
    This reverts commit 71375a7cdcddeed878718f294e64d366cde51069.
    
    # Conflicts:
    #	.vs/gitest/v16/.suo
    #	Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,12 @@ namespace gitest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+<<<<<<< HEAD
             Console.Beep(1500, 3000);
             Console.Beep(1500, 3000);
 
+=======
+>>>>>>> parent of 71375a7... add mwthod
         }
     }
 }
