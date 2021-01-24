1f6df05ea4371642ed28351e909f851043756c07
DFY73IER <leshaar14@gmail.com>
2021-01-24 19:19:12 +0100

    git init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
