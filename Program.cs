b494154d9bcd7d351cc8d60e75350bfdf7f0c535
DFY73IER <leshaar14@gmail.com>
2021-01-24 19:42:34 +0100

    Revert "add extrara"
    This reverts commit c46d9a81fc943db3c772a00b0a26762dc5984405.
    
    # Conflicts:
    #	Program.cs

--- Program.cs
+++ Program.cs
@@ -7,12 +7,14 @@ namespace gitest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
             Console.Beep(1500, 3000);
+<<<<<<< HEAD
             Console.Beep(1500, 3000);
 
 =======
 >>>>>>> parent of 71375a7... add mwthod
+=======
+>>>>>>> parent of c46d9a8... add extrara
         }
     }
 }
